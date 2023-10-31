85ace9a298769a871cf2f677e969896b385111de
tsubasa-alife <tsubasa.robotics@gmail.com>
2023-11-01 02:05:46 +0900

    ハイライトの切り替え機能サンプル

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -3,11 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Cell : MonoBehaviour, IPointerClickHandler
 {
+	[SerializeField] private GameObject highlight;
+	
+	
 	public void OnPointerClick(PointerEventData eventData)
 	{
 		Debug.Log("Cell clicked");
+		highlight.SetActive(!highlight.activeSelf);
 	}
 }
