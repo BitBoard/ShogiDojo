993aa72832ad19cf8a5eac5daf4c1821aa872e48
sgr <sagara-takashi-324@i.softbank.jp>
2023-11-15 00:27:27 +0900

    将棋盤のマス目を取得するスクリプト作成

--- Assets/Scripts/GameSceneScreen/GameSceneView.cs
+++ Assets/Scripts/GameSceneScreen/GameSceneView.cs
@@ -6,8 +6,11 @@ public class GameSceneView : MonoBehaviour
 	[SerializeField] private Button openDebugMenuButton;
 	[SerializeField] private Button closeDebugMenuButton;
 	[SerializeField] private GameObject debugMenu;
-	
-	public Button OpenDebugMenuButton => openDebugMenuButton;
+    [SerializeField] private GameObject board;
+
+    public Button OpenDebugMenuButton => openDebugMenuButton;
 	public Button CloseDebugMenuButton => closeDebugMenuButton;
 	public GameObject DebugMenu => debugMenu;
+
+	public GameObject Board => board;
 }
