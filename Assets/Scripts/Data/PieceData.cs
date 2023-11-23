99cb21f7924317c96ad39c5ff3e65ccf7c323b47
tsubasa-alife <tsubasa.robotics@gmail.com>
2023-11-23 17:51:24 +0900

    駒の種類のコンバート

--- Assets/Scripts/Data/PieceData.cs
+++ Assets/Scripts/Data/PieceData.cs
@@ -24,6 +24,44 @@ public class PieceData {
 
     public static PieceType StrToPieceType(string pieceTypeStr)
     {
-        return PieceType.WhitePawn;
+        // ここで文字列からswitch文でPieceTypeに変換する
+        // 例: "white_pawn" -> PieceType.WhitePawn
+        switch (pieceTypeStr)
+        {
+            case "white_pawn":
+                return PieceType.WhitePawn;
+            case "white_lance":
+                return PieceType.WhiteLance;
+            case "white_knight":
+                return PieceType.WhiteKnight;
+            case "white_silver":
+                return PieceType.WhiteSilver;
+            case "white_gold":
+                return PieceType.WhiteGold;
+            case "white_bishop":
+                return PieceType.WhiteBishop;
+            case "white_rook":
+                return PieceType.WhiteRook;
+            case "white_king":
+                return PieceType.WhiteKing;
+            case "black_pawn":
+                return PieceType.BlackPawn;
+            case "black_lance":
+                return PieceType.BlackLance;
+            case "black_knight":
+                return PieceType.BlackKnight;
+            case "black_silver":
+                return PieceType.BlackSilver;
+            case "black_gold":
+                return PieceType.BlackGold;
+            case "black_bishop":
+                return PieceType.BlackBishop;
+            case "black_rook":
+                return PieceType.BlackRook;
+            case "black_king":
+                return PieceType.BlackKing;
+            default:
+                return PieceType.WhitePawn;
+        }
     }
 }
--- Assets/Scripts/GameSceneScreen/GameSceneController.cs
+++ Assets/Scripts/GameSceneScreen/GameSceneController.cs
@@ -1,4 +1,3 @@
-using Assets.Scripts;
 using System;
 using UnityEngine;
 using UnityEngine.UI;
